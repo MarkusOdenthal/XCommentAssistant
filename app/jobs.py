99943d3cf6cb41582d5f0a5e0e004aa8575aa97f
Markus <markus.odenthal@real-digital.de>
2024-07-31 16:23:52 +0200

    refactor: Uncomment init_jobs function and add scheduled job for executing tasks every 3 seconds

--- app/jobs.py
+++ app/jobs.py
@@ -3,9 +3,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-#def init_jobs(scheduler):
-#    @scheduler.task('interval', id='do_job', seconds=3, misfire_grace_time=900)
-#    def job():
-#        print("Scheduled job executed")
-#        print(f"Time: {datetime.now()}")
-#        logger.info(f"Scheduled job executed at {datetime.now()}")
+# here I can then call the endpoints from
+def init_jobs(scheduler):
+    @scheduler.task('interval', id='do_job', seconds=3, misfire_grace_time=900)
+    def job():
+        print("Scheduled job executed")
+        print(f"Time: {datetime.now()}")
+        logger.info(f"Scheduled job executed at {datetime.now()}")
