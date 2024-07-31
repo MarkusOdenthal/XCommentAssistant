1e49deb33e990001d06f4ff21b8bef97d59333cf
Markus <markus.odenthal@real-digital.de>
2024-07-31 16:21:43 +0200

    refactor: Comment out unused code in init_jobs function

--- app/jobs.py
+++ app/jobs.py
@@ -3,9 +3,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def init_jobs(scheduler):
-    @scheduler.task('interval', id='do_job', seconds=3, misfire_grace_time=900)
-    def job():
-        print("Scheduled job executed")
-        print(f"Time: {datetime.now()}")
-        logger.info(f"Scheduled job executed at {datetime.now()}")
+#def init_jobs(scheduler):
+#    @scheduler.task('interval', id='do_job', seconds=3, misfire_grace_time=900)
+#    def job():
+#        print("Scheduled job executed")
+#        print(f"Time: {datetime.now()}")
+#        logger.info(f"Scheduled job executed at {datetime.now()}")
