42c9fcf9cf1cf2ac6a449f32b2a94d22928df4e1
Markus <markus.odenthal@real-digital.de>
2024-08-12 12:38:20 +0200

    refactor: Remove unused data.json and create_new_pinecone_index.py files

--- scripts/create_new_pinecone_index.py
+++
@@ -1,8 +0,0 @@
-from app.pinecone_client import create_index
-
-def main():
-    index_name = "x-posts-markus-odenthal"
-    create_index(index_name=index_name)
-
-if __name__ == "__main__":
-    main()
