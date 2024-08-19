2269e015c00449f0efa9e2f4d45449dae717ff0b
Markus <markus.odenthal@real-digital.de>
2024-08-19 09:58:43 +0200

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ import logging
 from flask import Flask
 from .routes import init_routes
 from config import Config
-from .langchain_setup import load_chains
 
 def create_app():
     app = Flask(__name__)
@@ -13,11 +12,6 @@ def create_app():
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
     logger = logging.getLogger(__name__)
-
-    # Load and set up chains
-    chains = load_chains()
-    app.config['viral_social_media_comments_ideas_chain'] = chains['viral_social_media_comments_ideas_chain']
-    app.config['viral_social_media_comments_refine_chain'] = chains['viral_social_media_comments_refine_chain']
 
     # Initialize routes
     init_routes(app)
--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,10 @@
 from flask import request
 from .x_client import initialize_twitter_client, get_tweet_statistics
 import logging
-from langsmith import Client
 
 logger = logging.getLogger(__name__)
 
 def init_routes(app):
-    @app.route("/add_label_data_to_topic_classification", methods=["POST"])
-    def add_label_data_to_topic_classification():
-        ls_client = Client()
-        try:
-            data = request.get_json()
-            post = data.get("post")
-            label = data.get("label")
-
-            if not post or not label:
-                return {"error": "Post and label are required"}, 400
-
-            _ = ls_client.create_examples(
-                inputs=[{"text": post}],
-                outputs=[{"label": label}],
-                dataset_name="XCommentClassification",
-            )
-            logger.info("Example added successfully")
-            return {"message": "Example added successfully"}, 200
-
-        except Exception as e:
-            logger.error(f"An error occurred: {str(e)}", exc_info=True)
-            return {"error": str(e)}, 500
-        
     @app.route("/tweet_statistics", methods=["POST"])
     def tweet_statistics():
         try:
